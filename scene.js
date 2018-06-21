50ec8848e70ab8ba9c415ae2ef229325659d87d9
Ahmed S Hassan <ahmad.ahmadsayed@gmail.com>
2018-06-22 00:31:36 +0200

    Fix broken connection in WS

--- scene.js
+++ scene.js
@@ -1,24 +1,30 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
     var path = require("path");
     var express = require("express");
     var transformation = require("./transformation");
     function SceneNode(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.name = n.name;
         this.sceneObjects = [];
-        var context = this.context(); 
+        var context = this.context();
         // To serve the view pages
         RED.httpNode.use("/scene", express.static(__dirname + '/scene-ui'));
         context.set("object", []);
         // To serve rest service that provide the html with the list of component
-        RED.httpNode.use("/sceneServe", function(req, res) {
+        RED.httpNode.use("/sceneServe", function (req, res) {
             res.send(context.get("object"));
         });
-        this.on('close', function(remove, done) {
-            context.get("object").clear();
-            done();
-        } )
-        transformation.dispatchTransformation({port: 9091});
-   }
-    RED.nodes.registerType("scene",SceneNode);
+        this.on('close', function (removed, done) {
+
+            transformation.terminateConnection(() => {
+                console.log('Connection Terminated');
+                context.set("object", []);
+                done();
+            });
+
+
+        })
+        transformation.dispatchTransformation({ port: 9099 });
+    }
+    RED.nodes.registerType("scene", SceneNode);
 }
--- transformation.js
+++ transformation.js
@@ -1,19 +1,20 @@
 const WebSocket = require('ws');
+var wss ;
 var queue = [];
 /**
  * 
  * @param {*} server 
  */
 function dispatchTransformation (server) {
-    const wss = new WebSocket.Server( server );
+    wss = new WebSocket.Server( server );
     wss.on('connection', function(ws, req) {
         ws.on('message', function(message) {
             console.log(message);
         });
         let interval = setInterval(function() {
-            ws.send("Hello World");
             if(queue.length > 0) {
                 queue.forEach(function(item) {
+                    console.log(item);
                     wss.send(item);
                 });
                 // no need to clear the array just reset the index
@@ -22,7 +23,13 @@ function dispatchTransformation (server) {
         }, 100);        
     });  
 }
+
+function terminateConnection (cb) {
+    console.log('Terminate');
+    wss.close( cb) ;
+}
 module.exports =  {
     dispatchTransformation, 
+    terminateConnection,
     get queue () {return queue;}
 }; 
