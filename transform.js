767f2499ee470d20e8dd5808028d233b49d1c259
Ahmed Hassan <ahmed.sayed.hassan@ibm.com>
2020-06-18 04:10:51 +0800

    Update transformation node to be able to get information from the dialog directly

--- transform.js
+++ transform.js
@@ -2,12 +2,18 @@ module.exports = function(RED) {
     function TransformNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+
         node.on('input', function(msg) {
             msg.payload = 
             {
                 "type": config.name,
                 "relative": config.relative,
-                "values": msg.payload
+                "values":  {
+                    x: config.xaxis != "" ? config.xaxis : msg.payload.x,
+                    y: config.yaxis != "" ? config.yaxis : msg.payload.y,
+                    z: config.zaxis != "" ? config.zaxis : msg.payload.z
+
+                }
             };
             node.send(msg);
         });
