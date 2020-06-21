566662589e9f06d9808be68dccdd6b7b3837b883
Ahmed Hassan <ahmed.sayed.hassan@ibm.com>
2020-06-22 01:52:34 +0800

    Adding Rotation

--- transform.js
+++ transform.js
@@ -1,22 +1,36 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
     function TransformNode(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         var node = this;
 
-        node.on('input', function(msg) {
-            msg.payload = 
-            {
-                "type": config.name,
-                "relative": config.relative,
-                "values":  {
-                    x: config.xaxis != "" ? config.xaxis : msg.payload.x,
-                    y: config.yaxis != "" ? config.yaxis : msg.payload.y,
-                    z: config.zaxis != "" ? config.zaxis : msg.payload.z
+        node.on('input', function (msg) {
+            if (config.name == "rotate") {
+                msg.payload =
+                {
+                    "type": config.name,
+                    "relative": config.relative,
+                    "values": {
+                        x: config.xaxis != "" ? ((Number(config.xaxis) * Math.PI)/180) : msg.payload.x,
+                        y: config.yaxis != "" ? ((Number(config.yaxis) * Math.PI)/180) : msg.payload.y,
+                        z: config.zaxis != "" ? ((Number(config.zaxis) * Math.PI)/180) : msg.payload.z
 
-                }
-            };
+                    }
+                };
+            } else {
+                msg.payload =
+                {
+                    "type": config.name,
+                    "relative": config.relative,
+                    "values": {
+                        x: config.xaxis != "" ? Number(config.xaxis) : msg.payload.x,
+                        y: config.yaxis != "" ? Number(config.yaxis) : msg.payload.y,
+                        z: config.zaxis != "" ? Number(config.zaxis) : msg.payload.z
+
+                    }
+                };
+            }
             node.send(msg);
         });
     }
-    RED.nodes.registerType("transform",TransformNode);
+    RED.nodes.registerType("transform", TransformNode);
 }
